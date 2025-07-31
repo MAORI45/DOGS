e4a96f36ab4233ba58bf0cec7e54d5d2dbcd384d
Света <montecumac@gmail.com>
2025-08-01 00:37:27 +0300

    Создана функция загрузки изображений

--- DOGS.py
+++ DOGS.py
@@ -1,11 +1,25 @@
 # http://dogs.ceo проект работа со ссылками
 
 import requests
-from tkinter import Tk, Toplevel, messagebox
+import tkinter
+from tkinter import Tk, Toplevel, messagebox, Button, Label
 from tkinter import ttk
 from PIL import Image, ImageTk
 from io import BytesIO
 
+def show_image():
+    image_url = get_dog_image() # отправляем функцию в функцию которая пришлет ссылку
+    if image_url: # если ссылка не пустая
+        try:
+            response = requests.get(image_url, stream=True) # что-то по ссылке
+            response.raise_for_status() # обрабатываем ошибку или статус ответа
+            img_data = BytesIO(response.content) # загрузка изображения в двоичном коде
+            img = Image.open(img_data) # открытие картинки
+            img.thumbnail((300, 300)) # размер картинки - ее сжатие?
+            label.config(image=img) # положить в метку картинку
+            label.image = img # чтобы картинка осталась в памяти
+        except Exception as e:
+            messagebox.showerror("Ошибка", f"Возникла ошибка: {e}")
 
 
 def extract_breed ():
@@ -32,7 +46,7 @@ window.geometry("360x420")
 label = Label()
 label.pack(pady=10)
 
-button = Button(text="Загрузить изображение", command=show_imedge)
+button = Button(text="Загрузить изображение", command=show_image)
 button.pack(pady=10)
 
 window.mainloop()
